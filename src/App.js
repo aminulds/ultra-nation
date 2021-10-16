275e915a0b3bdf016b18f774e0deeb82a9ac088f
Aminul Islam <aminulds61@gmail.com>
2021-10-16 07:53:53 +0600

    check api load

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  // state initialize
+  const [countries, setCountries] = useState([]);
+  // fetch api data
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+    .then((res) => res.json())
+    .then(data => setCountries(data));
+  }, []);
   return (
     <div className="App">
+      <h1>Data Loaded: {countries.length}</h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
